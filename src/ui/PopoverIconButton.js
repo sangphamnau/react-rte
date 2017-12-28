f5fc6a16df6b2c735a7dfcd02ff4db675effd475
sangphamnau <sang.pham@naustud.io>
2017-12-28 12:12:21 +0700

    Update PopoverIconButton.js

--- src/ui/PopoverIconButton.js
+++ src/ui/PopoverIconButton.js
@@ -6,6 +6,7 @@ import InputPopover from './InputPopover';
 import autobind from 'class-autobind';
 
 type Props = {
+  id: PropTypes.string,
   iconName: string;
   showPopover: boolean,
   onTogglePopover: Function,
@@ -15,30 +16,72 @@ type Props = {
 export default class PopoverIconButton extends Component {
   props: Props;
 
+  static defaultProps = {
+    id: 'upload-file',
+  }
+
   constructor() {
     super(...arguments);
     autobind(this);
   }
 
+  togglePopover = () => {
+    if (this.imageInput) {
+      this.imageInput.click();
+    }
+    this.props.onTogglePopover();
+  }
+
+  refInput = (input) => {
+    this.imageInput = input;
+  }
+
+  onChange = (e) => {
+    const { files } = e.target;
+    const reader = new FileReader();
+
+    reader.onload = (readerEvent) => {
+      const dataUrl = readerEvent.target.result;
+      this.props.onSubmit(dataUrl, files[0]);
+    };
+    reader.readAsDataURL(files[0]);
+  }
+
   render() {
-    let {onTogglePopover, showPopover, ...props} = this.props; // eslint-disable-line no-unused-vars
+    let {onTogglePopover, showPopover, id, ...props} = this.props; // eslint-disable-line no-unused-vars
     return (
-      <IconButton {...props} onClick={onTogglePopover}>
+      // <IconButton {...props} onClick={onTogglePopover}>
+      <IconButton htmlFor={this.props.id} {...props} onClick={this.togglePopover}>
         {this._renderPopover()}
       </IconButton>
     );
   }
 
   _renderPopover() {
-    if (!this.props.showPopover) {
+    if (this.props.iconName === 'image') {
+      return (
+        <input
+          type="file"
+          id={this.props.id} ref={this.refInput}
+          onChange={this.onChange}
+          style={{
+            visibility: 'hidden',
+            opacity: 0,
+            position: 'absolute',
+            zIndex: -1,
+          }}
+        />
+      );
+    } else if (!this.props.showPopover) {
       return null;
+    } else {
+      return (
+        <InputPopover
+          onSubmit={this._onSubmit}
+          onCancel={this._hidePopover}
+        />
+      );
     }
-    return (
-      <InputPopover
-        onSubmit={this._onSubmit}
-        onCancel={this._hidePopover}
-      />
-    );
   }
 
   _onSubmit() {
